54483c8d6e7685799122ef9a1c21300ee2365917
Navya <88023981+navysai1419@users.noreply.github.com>
2024-02-12 23:17:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,32 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
-	get name(){
-		return this.name;
-	}
-	set age(){
-		this.age=age;
-	}
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set age(age) {
+        this._age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${name} is studying`);
-	}
+    study() {
+        console.log(`${this.name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${name} is teaching`);
-	}
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
 }
 
 // Do not change the code below this line
